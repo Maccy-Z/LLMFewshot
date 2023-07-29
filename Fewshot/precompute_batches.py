18cc0cf03e9ce9dc8dc8e416e514833b715fc9b3
Your Name <you@example.com>
2023-07-29 14:36:15 +0100

    Move over

--- Fewshot/precompute_batches.py
+++ Fewshot/precompute_batches.py
@@ -2,7 +2,7 @@
 from dataloader import SplitDataloader
 import numpy as np
 import torch
-import os
+import os, sys
 import pickle
 
 data_dir = "./datasets/data"
@@ -52,29 +52,46 @@ class BatchConfig:
 # Save a batch to disk
 def save_batch(cfg, ds_name, num_batches):
     save_file = f"{data_dir}/{ds_name}/batches/meta_{cfg.N_meta}_targ_{cfg.N_target}"
-
     if not os.path.exists(f"{data_dir}/{ds_name}/batches"):
         os.makedirs(f"{data_dir}/{ds_name}/batches")
 
     try:
+        # A few datasets are too large. Restrict their sizes
+        if ds_name in ["semeion", "libras", "arrhythmia", "miniboone"]:
+            print(f"Reducing number of batches for dataset {ds_name}")
+            num_batches = num_batches // 2
+
         dl = SplitDataloader(cfg, datasets=[ds_name], bs=num_batches, testing=True)
-        batch = next(iter(dl))
+        raw_batch = next(iter(dl))
+
+        batch = [torch.stack(xs) for xs in raw_batch[:-1]]
+        batch.append(raw_batch[-1])
+
+        # Some entries take too much storage space. Downsize these.
+        batch[0] = batch[0].to(torch.float16)
+        batch[2] = batch[2].to(torch.float16)
+        batch[1] = batch[1].to(torch.int8)
+        batch[3] = batch[3].to(torch.int8)
+
+
+
         # Save format: num_rows, num_targets, num_cols
         with open(save_file, "wb") as f:
             pickle.dump(batch, f)
 
     except IndexError as e:
-        print(e)
+        # print(e)
         with open(save_file, "wb") as f:
             pickle.dump(None, f)
 
-
 def main():
     datasets = sorted([f for f in os.listdir(data_dir) if os.path.isdir(f'{data_dir}/{f}')])
-
+    #datasets = ["semeion"]
     N_batches = 2000
     for ds in datasets:
         for N_meta in [1, 2, 3, 5, 7]:
+            print(f'{ds =}, {N_meta = }')
+
             cfg = BatchConfig(N_meta=N_meta, N_target=5)
 
             save_batch(cfg, ds, N_batches)
